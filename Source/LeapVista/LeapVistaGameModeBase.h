9cc77e578ed5548998803c2a579ed338d82defae
RainSure <1114560341@qq.com>
2023-08-21 18:21:54 +0800

    initial commit

---
+++ Source/LeapVista/LeapVistaGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "LeapVistaGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LEAPVISTA_API ALeapVistaGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
